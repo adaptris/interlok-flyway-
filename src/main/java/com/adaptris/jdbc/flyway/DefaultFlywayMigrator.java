15621f7400407c7e98dbb36068508ad6eda08f39
Sebastien Belin <sebastien.belin@proagrica.com>
2023-05-26 16:39:49 +0900

    INTERLOK-4098 Uprade to Junit 5
    Upgrade gradle wrapper to 7.6.1

--- src/main/java/com/adaptris/jdbc/flyway/DefaultFlywayMigrator.java
+++ src/main/java/com/adaptris/jdbc/flyway/DefaultFlywayMigrator.java
@@ -1,35 +1,37 @@
 package com.adaptris.jdbc.flyway;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import javax.sql.DataSource;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.apache.commons.lang3.BooleanUtils;
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
+
 import com.adaptris.annotation.AdvancedConfig;
 import com.adaptris.annotation.AutoPopulated;
 import com.adaptris.annotation.ComponentProfile;
+import com.adaptris.annotation.DisplayOrder;
 import com.adaptris.annotation.InputFieldDefault;
 import com.adaptris.core.util.Args;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamImplicit;
-import org.flywaydb.core.api.configuration.FluentConfiguration;
-import com.adaptris.annotation.DisplayOrder;
-
-import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 /**
  * Perform a database migration using flyway
- * 
+ *
  * @config flyway-jdbc-migrator
  *
  */
 @ComponentProfile(summary = "Perform a database migration using flyway", since = "3.9.2")
 @XStreamAlias("flyway-jdbc-migrator")
-@DisplayOrder(order = {"flywayLocations", "flywayTable","baseline"})
+@DisplayOrder(order = { "flywayLocations", "flywayTable", "baseline" })
 public class DefaultFlywayMigrator implements FlywayMigrator {
 
   @XStreamImplicit(itemFieldName = "flyway-location")
@@ -45,15 +47,13 @@ public class DefaultFlywayMigrator implements FlywayMigrator {
   @InputFieldDefault(value = "false")
   private Boolean baseline;
 
-
   public DefaultFlywayMigrator() {
     setFlywayLocations(new ArrayList<>());
   }
 
   @Override
   public void migrate(DataSource source) throws Exception {
-    FluentConfiguration configuration = Flyway.configure().dataSource(source)
-            .locations(getFlywayLocations().toArray(new String[]{}));
+    FluentConfiguration configuration = Flyway.configure().dataSource(source).locations(getFlywayLocations().toArray(new String[] {}));
     if (!isEmpty(getFlywayTable())) {
       configuration.table(getFlywayTable());
     }
@@ -72,12 +72,12 @@ public class DefaultFlywayMigrator implements FlywayMigrator {
    * List of locations to scan recursively for migrations.
    *
    * <p>
-   * Locations can be prefixed with classpath or filesytem. which means that {@code filesystem:./sql/}
-   * if the files are on the filesystem or {@code classpath:migration/mysql} if they are on the
-   * classpath.
+   * Locations can be prefixed with classpath or filesytem. which means that {@code filesystem:./sql/} if the files are on the filesystem or
+   * {@code classpath:migration/mysql} if they are on the classpath.
    * </p>
    *
-   * @param flywayLocations the locations.
+   * @param flywayLocations
+   *          the locations.
    */
   public void setFlywayLocations(List<String> flywayLocations) {
     this.flywayLocations = Args.notNull(flywayLocations, "flywayLocations");
@@ -96,12 +96,11 @@ public class DefaultFlywayMigrator implements FlywayMigrator {
     return baseline;
   }
 
-
   /**
-   * Whether to automatically call baseline when migrate is executed against a non-empty schema with
-   * no schema history table.
+   * Whether to automatically call baseline when migrate is executed against a non-empty schema with no schema history table.
    *
-   * @param baseline true to baseline, default is false if not explicitly configured.
+   * @param baseline
+   *          true to baseline, default is false if not explicitly configured.
    */
   public void setBaseline(Boolean baseline) {
     this.baseline = baseline;
@@ -127,9 +126,9 @@ public class DefaultFlywayMigrator implements FlywayMigrator {
    * Optional schema history table. If provided flyway history will be created in this table.
    * </p>
    *
-   * @param flywayTable the table. optional. If not provided will use the default flyway history table
+   * @param flywayTable
+   *          the table. optional. If not provided will use the default flyway history table
    */
-
   public void setFlywayTable(String flywayTable) {
     this.flywayTable = Args.notNull(flywayTable, "flywayTable");
   }
@@ -138,4 +137,5 @@ public class DefaultFlywayMigrator implements FlywayMigrator {
     setFlywayTable(table);
     return this;
   }
+
 }
--- src/main/java/com/adaptris/jdbc/flyway/FlywayPluggableConnection.java
+++ src/main/java/com/adaptris/jdbc/flyway/FlywayPluggableConnection.java
@@ -1,6 +1,7 @@
 package com.adaptris.jdbc.flyway;
 
 import javax.validation.Valid;
+
 import com.adaptris.annotation.ComponentProfile;
 import com.adaptris.annotation.DisplayOrder;
 import com.adaptris.annotation.InputFieldDefault;
@@ -8,6 +9,7 @@ import com.adaptris.core.CoreException;
 import com.adaptris.core.jdbc.PluggableJdbcPooledConnection;
 import com.adaptris.core.util.ExceptionHelper;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,17 +21,13 @@ import lombok.Setter;
  *
  */
 @XStreamAlias("flyway-pluggable-jdbc-connection")
-@ComponentProfile(summary = "Extension of PluggableJdbcPooledConnection which will run flyway migration on init.",
-    tag = "connections,jdbc,flyway",
-    since = "3.9.2")
-@DisplayOrder(order = {"username", "password", "driverImp", "connectUrl", "flyway", "builder",
-    "poolProperties", "connectionProperties"})
+@ComponentProfile(summary = "Extension of PluggableJdbcPooledConnection which will run flyway migration on init.", tag = "connections,jdbc,flyway", since = "3.9.2")
+@DisplayOrder(order = { "username", "password", "driverImp", "connectUrl", "flyway", "builder", "poolProperties", "connectionProperties" })
 @NoArgsConstructor
 public class FlywayPluggableConnection extends PluggableJdbcPooledConnection {
 
   /**
    * Configure the migrator.
-   * 
    */
   @Valid
   @InputFieldDefault(value = "does nothing")
